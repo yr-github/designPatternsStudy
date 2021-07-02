5dbcd8b6bd5531293c5e9f10c235abfa878f69f7
adeleyr <adele.wyr@gmail.com>
2021-07-02 21:46:43 +0800

    学习准备

---
+++ BehavioralPatterns/Command/Command.h
@@ -0,0 +1,14 @@
+//
+// Created by adele_yr on 2021/7/2.
+//
+
+#ifndef DESIGNPATTERNSSTUDY_COMMAND_H
+#define DESIGNPATTERNSSTUDY_COMMAND_H
+
+
+class Command {
+
+};
+
+
+#endif //DESIGNPATTERNSSTUDY_COMMAND_H
---
+++ StructuralPatterns/adapter/adapter.h
@@ -0,0 +1,14 @@
+//
+// Created by adele_yr on 2021/7/2.
+//
+
+#ifndef DESIGNPATTERNSSTUDY_ADAPTER_H
+#define DESIGNPATTERNSSTUDY_ADAPTER_H
+
+
+class adapter {
+
+};
+
+
+#endif //DESIGNPATTERNSSTUDY_ADAPTER_H
